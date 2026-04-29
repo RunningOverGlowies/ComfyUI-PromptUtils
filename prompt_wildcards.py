7c49f315b431684aad02d322016338976a88de41
Running Over Glowies <100057094+RunningOverGlowies@users.noreply.github.com>
2026-04-29 06:54:56 +0100

    Add files via upload

--- prompt_wildcards.py
+++ prompt_wildcards.py
@@ -18,7 +18,7 @@ class PromptWildcards:
     def INPUT_TYPES(cls) -> Dict:
         return {
             "optional": {
-                "Prompt": ("STRING", {"multiline": True, "default": "Nationality: <nlt>\nSpecies: <species>\nOutfit: <out>\nOutfit (nsfw): <outn>\nMake-up: <makeup>\nExression: <exp>\nPose: <pose>\nLocation (mundane): <loc>\nLocation (weird): <locw>\nCelebrity: <celeb>\nMedia: <media>\nFilm: <film>\nGame: <game>\nWord: <word>\nPower-word: <wordp>\nPower-word (nsfw): <wordn>\nPassword: <pass>\nColor: <color>\nColor keyword: <color2>\nPhoto keyword: <wordk>\nMedium: <medi>\nTime of day: <time>\nLighting: <light>\nStyles: <style>\nYears: <69-79>, <19xx>, <2xxx>, <xxxx>"}),
+                "Prompt": ("STRING", {"multiline": True, "default": "Nationality: <nlt>\nSpecies: <species>\nOutfit: <out>\nOutfit (nsfw): <outn>\nMake-up: <makeup>\nExression: <exp>\nPose: <pose>\nLocation (mundane): <loc>\nLocation (weird): <locw>\nCelebrity: <celeb>\nMedia: <media>\nFilm: <film>\nGame: <game>\nWord: <word>\nPower-word: <wordp>\nPower-word (nsfw): <wordn>\nPassword: <pass>\nColor: <color>\nColor keyword: <color2>\nPhoto keyword: <wordk>\nMedium: <medi>\nTime of day: <time>\nLighting: <light>\nLighting (more): <light2>\nStyles: <style>\nStyles (more): <style2>\nPainting Style: <paintstyle>\nYears: <69-79>, <19xx>, <2xxx>, <xxxx>\nDanbooru Artist: <artist>\nPainter (with style): <painter>"}),
                 "seed": ("INT", {"default": random.randint(0, 30000), "min": 0, "max": 30000, "step": 1}),
             },
         }
@@ -29,7 +29,7 @@ class PromptWildcards:
     CATEGORY = "Prompt"
     
     def random_line_from_file(self, file_path):
-        with open(file_path, encoding='utf-8') as f:
+        with open(file_path, encoding='latin1') as f:
             line_count = sum(1 for _ in f)
             f.seek(0)
             random_line_index = self.rng.randint(0, line_count - 1)
@@ -43,7 +43,7 @@ class PromptWildcards:
             "<celeb>": "celebrities", "<media>": "assorted_media", "<game>": "games", "<film>": "movies", "<species>": "species", "<nlt>": "nationalities", 
             "<loc>": "locations_mundane", "<locw>": "locations_weird", "<out>": "outfits", "<outn>": "outfits_nsfw",
             "<makeup>": "makeup", "<exp>": "expressions", "<time>": "times", "<pose>": "poses", 
-            "<pass>": "passwords", "<word>": "words", "<wordp>": "power_words", "<wordn>": "power_words_nsfw", "<wordk>": "keywords", "<color>": "colors","<color2>": "color2","<medi>": "mediums","<light>":"lighting","<style>":"styles"}.items()}
+            "<pass>": "passwords", "<word>": "words", "<wordp>": "power_words", "<wordn>": "power_words_nsfw", "<wordk>": "keywords", "<color>": "colors","<color2>": "color2","<medi>": "mediums","<light>":"lighting","<light2>":"lighting2","<style>":"styles","<style2>":"styles2","<artist>":"artists","<painter>":"artist_styles","<paintstyle>":"painting_styles"}.items()}
         while any(tag in Prompt for tag in files):
             Prompt = re.sub("|".join(map(re.escape, files)), lambda m: self.random_line_from_file(files[m.group()]), Prompt, 1)
         return (re.sub(r"<(\d+)-(\d+)>|<(\d{2}xx|xxxx|\d{1}xxx)>", lambda m: str(self.rng.randint(1900 + int(m.group(1)), 1900 + int(m.group(2)))) if m.group(1) else str(self.rng.randint(1000, 9999) if m.group(3) == "xxxx" else self.rng.randint(int(m.group(3)[0] + "000"), int(m.group(3)[0] + "999")) if "xxx" in m.group(3) else self.rng.randint(int(m.group(3)[:2] + "00"), int(m.group(3)[:2] + "99"))), Prompt),)
